71fa0ef5a7169c02c3b8ed70d362877eb724f743
Debabrata-DevOps <debubarui12345@gmail.com>
2023-09-09 20:49:36 +0530

    Commit for 21-Exception-Handling

---
+++ pythonProject/MyPyProject/21-Exception-Handling.py
@@ -0,0 +1,17 @@
+'''
+Exception Handling :-
+    try Block{
+        This is the actual business code
+
+        if try block has any exception{
+            except or catch block will be executed and here exception will be highlighted
+         }
+        else{
+            else block will be highlighted
+        }
+    }
+    finally block will be executed everytime if try block has any exception or doesn't have any exception
+
+'''
+
+
